7ed07252c49f7456cbd78bb2aa8d1f706f56d3d9
drawdlu <152858099+drawdlu@users.noreply.github.com>
2025-08-23 18:39:21 +0800

    feat: Add database validations for kittens

--- app/models/kitten.rb
+++ app/models/kitten.rb
@@ -1,2 +1,6 @@
 class Kitten < ApplicationRecord
+  validates :name, presence: true
+  validates :age, presence: true, numericality: { in: 1..25 }
+  validates :cuteness, presence: true, numericality: { in: 1..10 }
+  validates :softness, presence: true, numericality: { in: 1..10 }
 end
